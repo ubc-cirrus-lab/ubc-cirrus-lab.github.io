d2e1337ae2b7dedfce0d67d6d756d6588d4580d0
Mohammad Shahrad <engshahrad@users.noreply.github.com>
2023-11-06 11:17:13 -0800

    news section updated

--- all-news.js
+++ all-news.js
@@ -1,6 +1,9 @@
 document.write('\
 <ul style="text-align: left;vertical-align: top;">\
 <li>\
+November \'23 - Viktor from ETHZ joined us as a vising Master\'s student! He is a <a href="https://www.mitacs.ca/en/programs/globalink/globalink-research-award" target="_blank">Mitacs GRA</a> recepient.\
+</li>\
+<li>\
 October \'23 - Changyuan receives the UBC GSI award for 2023. He is already a <a href="https://www.grad.ubc.ca/awards/four-year-doctoral-fellowship-4yf" target="_blank">UBC 4YF</a> scholar!\
 </li>\
 <li>\
--- recent-news.js
+++ recent-news.js
@@ -1,6 +1,9 @@
 document.write('\
 <ul style="text-align: left;vertical-align: top;">\
 <li>\
+November \'23 - Viktor from ETHZ joined us as a vising Master\'s student! He is a <a href="https://www.mitacs.ca/en/programs/globalink/globalink-research-award" target="_blank">Mitacs GRA</a> recepient.\
+</li>\
+<li>\
 October \'23 - Changyuan receives the UBC GSI award for 2023. He is already a <a href="https://www.grad.ubc.ca/awards/four-year-doctoral-fellowship-4yf" target="_blank">UBC 4YF</a> scholar!\
 </li>\
 <li>\
@@ -11,9 +14,6 @@ September \'23 - <a href="./papers/socc23_parrotfish.pdf" target="_blank">Our pa
 </li>\
 <li>\
 May \'23 - Team secures two papers in <a href="https://www.usenix.org/conference/atc23" target="_blank">USENIX ATC \'23</a>, including one in collaboration with <a href="https://sns.cs.princeton.edu/" target="_blank">Princeton SNS group</a>.\
-</li>\
-<li>\
-September \'22 - Joe Hattori joins as visiting research student from the University of Tokyo. (more on the <a href="https://global.ubc.ca/go-global/coming-ubc-study-abroad/visiting-international-research-students-virs" target="_blank">UBC VIRS program</a>)\
 </li>\
 </ul>\
 ');
