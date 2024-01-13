f4d9e34101cee696f38d2a286fa61b3f014eb157
Mohammad Shahrad <engshahrad@users.noreply.github.com>
2024-01-13 13:05:19 -0800

    minor fix

--- recent-news.js
+++ recent-news.js
@@ -1,7 +1,7 @@
 document.write('\
 <ul style="text-align: left;vertical-align: top;">\
 <li>\
-December \'23 - Ghazal successfully defended her MASc thesis defense!\
+December \'23 - Congrats to Ghazal for successfully defending her MASc thesis!\
 </li>\
 <li>\
 November \'23 - Viktor from ETHZ joined us as a vising Master\'s student! He is a <a href="https://www.mitacs.ca/en/programs/globalink/globalink-research-award" target="_blank">Mitacs GRA</a> recepient.\
