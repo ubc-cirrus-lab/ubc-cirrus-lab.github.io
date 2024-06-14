c8af504ab9648b43d788d079e9f88fca4298dc42
Mohammad Shahrad <engshahrad@users.noreply.github.com>
2024-06-14 12:43:36 -0700

    recent lab news

--- all-news.js
+++ all-news.js
@@ -1,6 +1,12 @@
 document.write('\
 <ul style="text-align: left;vertical-align: top;">\
 <li>\
+June \'24 - Changyuan\'s paper is accepted to <a href="https://hotcarbon.org/" target="_blank">HotCarbon \'24</a>!\
+</li>\
+<li>\
+June \'24 - Arshia\'s talk proposal accepted to <a href="https://www.serverlesscomputing.org/woscx3/index.html" target="_blank">WOSCx3</a>! He\'ll present on June 19th.\
+</li>\
+<li>\
 April \'24 - Changyuan receives the prestigious <a href="https://www.nserc-crsng.gc.ca/Students-Etudiants/PG-CS/CGSD-BESCD_eng.asp" target="_blank">CGS-D</a> scholarship.\
 </li>\
 <li>\
--- recent-news.js
+++ recent-news.js
@@ -1,6 +1,12 @@
 document.write('\
 <ul style="text-align: left;vertical-align: top;">\
 <li>\
+June \'24 - Changyuan\'s paper is accepted to <a href="https://hotcarbon.org/" target="_blank">HotCarbon \'24</a>!\
+</li>\
+<li>\
+June \'24 - Arshia\'s talk proposal accepted to <a href="https://www.serverlesscomputing.org/woscx3/index.html" target="_blank">WOSCx3</a>! He\'ll present on June 19th.\
+</li>\
+<li>\
 April \'24 - Changyuan receives the prestigious <a href="https://www.nserc-crsng.gc.ca/Students-Etudiants/PG-CS/CGSD-BESCD_eng.asp" target="_blank">CGS-D</a> scholarship.\
 </li>\
 <li>\
@@ -8,12 +14,6 @@ April \'24 - Changyuan receives the BC Graduate Scholarships.\
 </li>\
 <li>\
 January \'24 - Arshia and Daniel receive UBC GSI awards!\
-</li>\
-<li>\
-January \'24 - Nalin and Nima won the 3rd Place Distinguished Poster Award at the 2024 ECE Research Day!\
-</li>\
-<li>\
-December \'23 - Congrats to Ghazal for successfully defending her MASc thesis!\
 </li>\
 </ul>\
 ');
