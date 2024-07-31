30e5a0b3d451d91d3ef0102d13666168ab37be26
Mohammad Shahrad <engshahrad@users.noreply.github.com>
2024-07-31 11:59:16 -0700

    link to Arshia's talk added

--- recent-news.js
+++ recent-news.js
@@ -7,7 +7,7 @@ July \'24 - Congrats to Nalin for successfully defending his MASc thesis! (<a hr
 June \'24 - Changyuan\'s paper is accepted to <a href="https://hotcarbon.org/" target="_blank">HotCarbon \'24</a>!\
 </li>\
 <li>\
-June \'24 - Arshia\'s talk proposal accepted to <a href="https://www.serverlesscomputing.org/woscx3/index.html" target="_blank">WOSCx3</a>! He\'ll present on June 19th.\
+June \'24 - Arshia gave a talk at the <a href="https://www.serverlesscomputing.org/woscx3/index.html" target="_blank">WOSCx3</a>! (<a href="https://www.serverlesscomputing.org/woscx3/presentations/6-ArshiaMoghimi-Parrotfish.pdf" target="_blank">slides</a>).\
 </li>\
 <li>\
 April \'24 - Changyuan receives the prestigious <a href="https://www.nserc-crsng.gc.ca/Students-Etudiants/PG-CS/CGSD-BESCD_eng.asp" target="_blank">CGS-D</a> scholarship.\
