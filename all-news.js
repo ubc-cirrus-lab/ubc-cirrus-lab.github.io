d18cf370aaf61767ea85414e50286d25b0ce696e
Mohammad Shahrad <engshahrad@users.noreply.github.com>
2024-09-15 11:11:16 -0700

    recent updates

--- all-news.js
+++ all-news.js
@@ -1,6 +1,12 @@
 document.write('\
 <ul style="text-align: left;vertical-align: top;">\
 <li>\
+September \'24 - Congrats to Arshia for successfully defending his MASc thesis! (<a href="https://www.flickr.com/photos/194704428@N02/53997526399/in/datetaken-public/" target="_blank">photo</a>)\
+</li>\
+<li>\
+September \'24 - Our paper is accepted to <a href="https://sp2025.ieee-security.org/" target="_blank">IEEE S&P (Oakland)</a>!\
+</li>\
+<li>\
 September \'24 - Shuo Zhang joins our team, and already won the UBC BPOC Graduate Excellence Award!\
 </li>\
 <li>\
--- recent-news.js
+++ recent-news.js
@@ -1,6 +1,12 @@
 document.write('\
 <ul style="text-align: left;vertical-align: top;">\
 <li>\
+September \'24 - Congrats to Arshia for successfully defending his MASc thesis! (<a href="https://www.flickr.com/photos/194704428@N02/53997526399/in/datetaken-public/" target="_blank">photo</a>)\
+</li>\
+<li>\
+September \'24 - Our paper is accepted to <a href="https://sp2025.ieee-security.org/" target="_blank">IEEE S&P (Oakland)</a>!\
+</li>\
+<li>\
 September \'24 - Shuo Zhang joins our team, and already won the UBC BPOC Graduate Excellence Award!\
 </li>\
 <li>\
@@ -8,12 +14,6 @@ August \'24 - Our team has a paper accepted to <a href="https://sigops.org/s/con
 </li>\
 <li>\
 July \'24 - Congrats to Nalin for successfully defending his MASc thesis! (<a href="https://www.flickr.com/photos/194704428@N02/53838355452/in/datetaken-public/" target="_blank">photo</a>)\
-</li>\
-<li>\
-June \'24 - Changyuan\'s paper is accepted to <a href="https://hotcarbon.org/" target="_blank">HotCarbon \'24</a>!\
-</li>\
-<li>\
-June \'24 - Arshia gave a talk at the <a href="https://www.serverlesscomputing.org/woscx3/index.html" target="_blank">WOSCx3</a>! (<a href="https://www.serverlesscomputing.org/woscx3/presentations/6-ArshiaMoghimi-Parrotfish.pdf" target="_blank">slides</a>).\
 </li>\
 </ul>\
 ');
