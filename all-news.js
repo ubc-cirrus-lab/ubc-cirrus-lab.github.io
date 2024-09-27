83f18c4134cdea8b8783f118be319796c2c1f055
Mohammad Shahrad <engshahrad@users.noreply.github.com>
2024-09-26 23:20:25 -0700

    updated recent news

--- all-news.js
+++ all-news.js
@@ -1,6 +1,9 @@
 document.write('\
 <ul style="text-align: left;vertical-align: top;">\
 <li>\
+September \'24 - Our vision paper with MIT and UMass researchers is accepted to <a href="https://acmsocc.org/2024/" target="_blank">SoCC \'24</a>.\
+</li>\
+<li>\
 September \'24 - Congrats to Arshia for successfully defending his MASc thesis! (<a href="https://www.flickr.com/photos/194704428@N02/53997526399/in/datetaken-public/" target="_blank">photo</a>)\
 </li>\
 <li>\
@@ -10,7 +13,7 @@ September \'24 - Our paper is accepted to <a href="https://sp2025.ieee-security.
 September \'24 - Shuo Zhang joins our team, and already won the UBC BPOC Graduate Excellence Award!\
 </li>\
 <li>\
-August \'24 - Our team has a paper accepted to <a href="https://sigops.org/s/conferences/sosp/2024/" target="_blank">SOSP \'24</a>!\
+August \'24 - Our team has a <a href="./papers/sosp24_caribou.pdf">paper</a> accepted to <a href="https://sigops.org/s/conferences/sosp/2024/" target="_blank">SOSP \'24</a>!\
 </li>\
 <li>\
 July \'24 - Congrats to Nalin for successfully defending his MASc thesis! (<a href="https://www.flickr.com/photos/194704428@N02/53838355452/in/datetaken-public/" target="_blank">photo</a>)\
