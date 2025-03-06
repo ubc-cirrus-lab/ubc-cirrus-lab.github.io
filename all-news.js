ab564c6fe2f679c573d94760f7a39fbcd1027b67
Mohammad Shahrad <engshahrad@users.noreply.github.com>
2025-03-05 22:47:03 -0800

    updated news

--- all-news.js
+++ all-news.js
@@ -1,13 +1,16 @@
 document.write('\
 <ul style="text-align: left;vertical-align: top;">\
 <li>\
+February \'25 - Congrats to Hanan and Changyuan for their accepted <a href="https://sesame25.github.io/" target="_blank">SESAME</a> workshop <a href="./papers/sesame25_awwad.pdf" target="_blank">paper</a>.\
+</li>\
+<li>\
 January \'25 - Welcoming Hanan Awwad to the team!\
 </li>\
 <li>\
 December \'24 - Congrats to Nima on defending his MASc thesis!\
 </li>\
 <li>\
-September \'24 - Our vision paper with MIT and UMass researchers is accepted to <a href="https://acmsocc.org/2024/" target="_blank">SoCC \'24</a>.\
+September \'24 - Our <a href="./papers/socc24-sunk-carbon-fallacy.pdf" target="_blank">vision paper</a> with MIT and UMass researchers is accepted to <a href="https://acmsocc.org/2024/" target="_blank">SoCC \'24</a>.\
 </li>\
 <li>\
 September \'24 - Congrats to Arshia for successfully defending his MASc thesis! (<a href="https://www.flickr.com/photos/194704428@N02/53997526399/in/datetaken-public/" target="_blank">photo</a>)\
