4104d0f7c5e535cd32abdce29a8c90384a66df87
Mohammad Shahrad <engshahrad@users.noreply.github.com>
2025-06-18 17:27:41 -0700

    recent news

--- all-news.js
+++ all-news.js
@@ -1,6 +1,9 @@
 document.write('\
 <ul style="text-align: left;vertical-align: top;">\
 <li>\
+June \'25 - Praveen gave a talk at the <a href="https://www.serverlesscomputing.org/woscx4/program.html" target="_blank">WOSCx4</a>. (<a href="https://www.serverlesscomputing.org/woscx4/presentations/t5-WoSC-Praveen%20Gupta-Growlithe_A%20Developer-Centric%20Compliance%20Tool%20for%20Serverless%20Applications.pdf" target="_blank">slides</a>).\
+</li>\
+<li>\
 February \'25 - Congrats to Hanan and Changyuan for their accepted <a href="https://sesame25.github.io/" target="_blank">SESAME</a> workshop <a href="./papers/sesame25_awwad.pdf" target="_blank">paper</a>.\
 </li>\
 <li>\
--- recent-news.js
+++ recent-news.js
@@ -1,6 +1,9 @@
 document.write('\
 <ul style="text-align: left;vertical-align: top;">\
 <li>\
+June \'25 - Praveen gave a talk at the <a href="https://www.serverlesscomputing.org/woscx4/program.html" target="_blank">WOSCx4</a>. (<a href="https://www.serverlesscomputing.org/woscx4/presentations/t5-WoSC-Praveen%20Gupta-Growlithe_A%20Developer-Centric%20Compliance%20Tool%20for%20Serverless%20Applications.pdf" target="_blank">slides</a>).\
+</li>\
+<li>\
 February \'25 - Congrats to Hanan and Changyuan for their accepted <a href="https://sesame25.github.io/" target="_blank">SESAME</a> workshop <a href="./papers/sesame25_awwad.pdf" target="_blank">paper</a>.\
 </li>\
 <li>\
@@ -11,9 +14,6 @@ December \'24 - Congrats to Nima on defending his MASc thesis!\
 </li>\
 <li>\
 September \'24 - Our <a href="./papers/socc24-sunk-carbon-fallacy.pdf" target="_blank">vision paper</a> with MIT and UMass researchers is accepted to <a href="https://acmsocc.org/2024/" target="_blank">SoCC \'24</a>.\
-</li>\
-<li>\
-September \'24 - Congrats to Arshia for successfully defending his MASc thesis! (<a href="https://www.flickr.com/photos/194704428@N02/53997526399/in/datetaken-public/" target="_blank">photo</a>)\
 </li>\
 </ul>\
 ');
